cd92608ee2c172f04a847d4ef045c4aea85d1607
Lộc Nguyễn <locnguyen@Luke-Ns-MacBook-Pro.local>
2025-10-16 13:42:47 +1100

    Initialize Wagtail + Next.js project

---
+++ backend/core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nh^w+tp1n+04b8%2jf8&v5o+&$b__&za4r^4=924-9rdoi-et0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
